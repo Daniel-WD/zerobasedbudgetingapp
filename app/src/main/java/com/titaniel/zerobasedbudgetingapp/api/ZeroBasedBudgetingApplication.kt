ce1df1a50f51c2bcdbf597cc1e36092800292cce
Daniel Weidensdörfer <daniel.weidensdoerfer@gmail.com>
2021-02-19 20:25:31 +0100

    Application file doc #44

--- app/src/main/java/com/titaniel/zerobasedbudgetingapp/api/ZeroBasedBudgetingApplication.kt
+++ app/src/main/java/com/titaniel/zerobasedbudgetingapp/api/ZeroBasedBudgetingApplication.kt
@@ -3,6 +3,8 @@ package com.titaniel.zerobasedbudgetingapp.api
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+ * Application file, needed for Hilt
+ */
 @HiltAndroidApp
-class ZeroBasedBudgetingApplication : Application() {
-}
+class ZeroBasedBudgetingApplication : Application()
