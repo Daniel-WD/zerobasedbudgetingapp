3b3be0780a3afcde295a0a76e0f8d42d8bc90181
Daniel Weidensdörfer <daniel.weidensdoerfer@gmail.com>
2021-04-01 18:41:50 +0200

    Add KotlinExtensionsTest #12

--- app/src/main/java/com/titaniel/zerobasedbudgetingapp/utils/KotlinExtensions.kt
+++ app/src/main/java/com/titaniel/zerobasedbudgetingapp/utils/KotlinExtensions.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.MutableLiveData
 
 /**
  * Add [element] to the list, if the value of the property returned by [uniquePropertyDelegate] will be unique.
+ * Null is treated as normal value.
  */
 fun <E, P> MutableList<E>.addUnique(element: E, uniquePropertyDelegate: (E) -> P): Boolean {
-    find { uniquePropertyDelegate(it) == uniquePropertyDelegate(element) }?.let {
+    find { uniquePropertyDelegate(it) == uniquePropertyDelegate(element) } ?: run {
         add(element)
         return true
     }
