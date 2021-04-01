bd337067bceab55950eeb356daa9dc3ffc050a7c
Daniel Weidensdörfer <daniel.weidensdoerfer@gmail.com>
2021-04-01 17:44:11 +0200

    Add new DatabaseAttributeConverters tests #12

--- app/src/test/java/com/titaniel/zerobasedbudgetingapp/utils/DatabaseAttributeConvertersTest.kt
+++ app/src/test/java/com/titaniel/zerobasedbudgetingapp/utils/DatabaseAttributeConvertersTest.kt
@@ -3,6 +3,7 @@ package com.titaniel.zerobasedbudgetingapp.utils
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 import java.time.LocalDate
+import java.time.YearMonth
 
 class DatabaseAttributeConvertersTest {
 
@@ -19,6 +20,16 @@ class DatabaseAttributeConvertersTest {
     @Test
     fun converts_date_to_epoch_day_correctly() {
         assertThat(converters.localDateToEpochDay(LocalDate.of(2021, 2, 20))).isEqualTo(18678)
+    }
+
+    @Test
+    fun converts_epoch_day_to_year_month_correctly() {
+        assertThat(converters.epochDayToYearMonth(18710)).isEqualTo(YearMonth.of(2021, 3))
+    }
+
+    @Test
+    fun converts_year_month_to_epoch_day_correctly() {
+        assertThat(converters.yearMonthToEpochDay(YearMonth.of(2021, 2))).isEqualTo(18686)
     }
 
 }
